10accbee3ebcfaee3cf806f4b4c2156d108c7f51
Sydnec <97565235+Sydnec@users.noreply.github.com>
2023-11-23 16:14:17 +0100

    Changer le message d'attribution des rôle à chaque redemmarage

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,12 @@
 import { log } from "../../modules/utils.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const name = "ready";
 const once = true;
 async function execute(bot) {
 	log(`Bonjour, je suis ${bot.user.tag} et j'ai bien démarré !`);
+	bot.handleUpdateRoleMessage();
 }
 
 export { name, once, execute };
