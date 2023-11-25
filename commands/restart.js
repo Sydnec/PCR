5eef5d3eec743412811c13a1c48b1c3a8891adf0
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-11-25 22:13:09 +0100

    Ajout d'une fonction restart

---
+++ commands/restart.js
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { handleException, log, error, isAdmin } from '../modules/utils.js';
+import dotenv from 'dotenv';
+dotenv.config(); // process.env.CONSTANT
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('restart')
+		.setDescription('Redémarre le bot (réservé aux administrateurs)'),
+
+	async execute(interaction) {
+		try {
+			if (!isAdmin(interaction.member)) {
+				await interaction.reply({
+					content:
+						"Vous n'avez pas les autorisations nécessaires pour utiliser cette commande.",
+					ephemeral: true,
+				});
+				return;
+			} else {
+				await interaction.reply({
+					content: 'Redémarrage en cours...',
+					ephemeral: true,
+				});
+				await bot.destroy();
+				process.exit(0);
+			}
+		} catch (e) {
+			handleException(e);
+		}
+	},
+};
