74e900b4dbb72802bc53e84afc411a977986a8b4
Sydnec <simon.bourlier@etu.mines-ales.fr>
2023-12-03 20:19:58 +0100

    Compter les messages

--- commands/projet.js
+++ commands/projet.js
@@ -1,66 +1,64 @@
-import { SlashCommandBuilder } from "discord.js";
-import { handleException, isAdmin } from "../modules/utils.js";
-import dotenv from "dotenv";
+import { SlashCommandBuilder } from 'discord.js';
+import { handleException, isAdmin } from '../modules/utils.js';
+import dotenv from 'dotenv';
 dotenv.config(); // process.env.CONSTANT
 
 export default {
-  data: new SlashCommandBuilder().setName("projet").setDescription("projet"),
+    data: new SlashCommandBuilder().setName('projet').setDescription('projet'),
 
-  async execute(interaction) {
-    try {
-      if (isAdmin(interaction.member)) {
-        interaction.reply({
-          content:
-            "Le récapitulatif 2023 te sera envoyé en MP quand il sera prêt",
-          ephemeral: true,
-        });
-
-        // Récupérer les informations nécessaires
-        const member = interaction.member;
-        const joinDate = member.joinedAt.toLocaleDateString("fr-FR", {
-          day: "numeric",
-          month: "numeric",
-          year: "numeric",
-        });
-        processAllMembers(interaction.guild, member);
-      }
-    } catch (error) {
-      handleException(error);
-    }
-  },
+    async execute(interaction) {
+        try {
+            if (isAdmin(interaction.member)) {
+                interaction.reply({
+                    content:
+                        'Le récapitulatif 2023 te sera envoyé en MP quand il sera prêt',
+                    ephemeral: true,
+                });
+                getMessageCount(interaction.guild);
+            }
+        } catch (error) {
+            handleException(error);
+        }
+    },
 };
 
-async function processAllMembers(guild, memberToSend) {
-  const members = guild.members.cache;
+async function getMessageCount(guild) {
+    try {
+        await guild.members.fetch(); // Assure-toi que tous les membres sont chargés
 
-  for (const [_, member] of members) {
-    const messageCount = await getMessageCount(guild, member);
-    await memberToSend.send(`Le nombre de messages de ${member.user.username} est ${messageCount}`);
-  }
-}
+        const userMessageCount = {}; // Utilise un objet pour stocker le nombre de messages par utilisateur
 
-async function getMessageCount(guild, member) {
-  const channels = guild.channels.cache;
-  const excludedChannelTypes = [2, 4, 15];
-  let count = 0;
+        for (const [_, channel] of guild.channels.cache) {
+            const excludedChannelTypes = [4, 15];
 
-  for (const [_, channel] of channels) {
-    if (!excludedChannelTypes.includes(channel.type)) {
-      try {
-        let lastId;
-        do {
-          const options = { limit: 100, before: lastId };
-          const messages = await channel.messages.fetch(options);
-          const filteredMessages = messages.filter(
-            (msg) => msg.author.id === member.id
-          );
-          count += filteredMessages.size;
-          lastId = messages.last()?.id;
-        } while (lastId !== undefined);
-      } catch (e) {
+            if (!excludedChannelTypes.includes(channel.type)) {
+                try {
+                    let lastId;
+                    do {
+                        const options = { limit: 100, before: lastId };
+                        const messages = await channel.messages.fetch(options);
+
+                        for (const [_, member] of guild.members.cache) {
+                            const filteredMessages = messages.filter(
+                                (msg) => msg.author.id === member.id
+                            );
+                            userMessageCount[member.id] =
+                                (userMessageCount[member.id] || 0) +
+                                filteredMessages.size;
+                        }
+
+                        lastId = messages.last()?.id;
+                    } while (lastId !== undefined);
+                } catch (e) {
+                    handleException(e);
+                }
+            }
+        }
+        console.log(userMessageCount)
+        // Maintenant, userMessageCount contient le nombre de messages pour chaque utilisateur
+        return userMessageCount;
+    } catch (e) {
         handleException(e);
-      }
+        return {}; // En cas d'erreur, retourne un objet vide
     }
-  }
-  return count;
 }
