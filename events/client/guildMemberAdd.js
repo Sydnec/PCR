8660012f81d52f13e33612b449cd74a0f9826833
Sydnec <simon.bourlier@etu.mines-ales.fr>
2024-02-11 23:36:59 +0100

    retour à la normal

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -3,26 +3,8 @@ dotenv.config();
 
 const name = 'guildMemberAdd';
 const once = false;
-async function execute(member, bot) {
-    // Récupère les invitations du serveur
-    const guildInvites = await member.guild.invites.fetch();
-
-    // Trouve l'invitation utilisée
-    const usedInvite = guildInvites.find(inv => guildInvites.get(inv.code));
-
-    let role;
-    if (usedInvite) {
-        switch (usedInvite.code) {
-            case process.env.TEMPORY_MEMBER_INVITE_CODE:
-                role = member.guild.roles.cache.get(process.env.TEMPORY_MEMBER_ROLE_ID);
-                break;
-        
-            default:
-                role = member.guild.roles.cache.get(process.env.DEFAULT_ROLE_ID);
-                break;
-        }
-        
-    }
+async function execute(member) {
+    let role = member.guild.roles.cache.get(process.env.DEFAULT_ROLE_ID);
 
     if (role) {
         member.roles.add(role);
