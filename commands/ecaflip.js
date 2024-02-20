233d15f30b3270e95ef84226ebde1adbd4e39d9d
Sydnec <simon.bourlier@etu.mines-ales.fr>
2024-02-20 22:32:56 +0100

    10 choix max sur la commande ecaflip

--- commands/ecaflip.js
+++ commands/ecaflip.js
@@ -3,31 +3,45 @@ import { handleException } from '../modules/utils.js';
 import dotenv from 'dotenv';
 dotenv.config(); // process.env.CONSTANT
 
-export default {
-	data: new SlashCommandBuilder()
-		.setName('ecaflip')
-		.setDescription('Pile ou Face ?')
-		.addStringOption((option) =>
-			option
-				.setName('option1')
-				.setDescription('Choix 1 ou question à répondre par Oui ou Non')
-				.setRequired(true)
-		)
-		.addStringOption((option) =>
-			option.setName('option2').setDescription('Choix 2')
-		),
+const data = new SlashCommandBuilder()
+	.setName('ecaflip')
+	.setDescription('Pile ou Face ?')
+	.addStringOption((option) =>
+		option
+			.setName('option1')
+			.setDescription('Option 1 ou question à répondre par Oui ou Non')
+			.setRequired(true)
+	);
 
+for (let i = 2; i <= 10; i++) {
+	data.addStringOption((option) =>
+		option
+			.setName(`option${i}`)
+			.setDescription(
+				`Option ${i}`
+			)
+			.setRequired(false)
+	);
+}
+
+export default {
+	data,
 	async execute(interaction) {
-		const option2 = interaction.options.getString('option2');
-		if (Math.random() > 0.5) {
-            interaction.reply({
-                content: option2 == '' ? 'Oui' : interaction.options.getString('option1'),
-            });
-		} else {
-            interaction.reply({
-                content: option2 == '' ? 'Non' : option2,
-            });
+		let options = []
+		for(let i = 1; i <= 10; i++){
+			const option = interaction.options.getString(`option${i}`);
+			if(option != '') options.push(option);
 		}
-		
+		const alea = Math.random();
+		const optionNumber = Math.floor(alea * options.length);
+		if (options.length) {
+			interaction.reply({
+				content: alea >= 0.5 ? 'Oui' : 'Non',
+			});
+		} else {
+			interaction.reply({
+				content: options[optionNumber],
+			});
+		}
 	},
 };
