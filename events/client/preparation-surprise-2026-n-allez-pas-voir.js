f39297121aeb5db8a70f1b0eeb896d1656fb838a
Sydnec <sydnec@debian.dev.local>
2025-08-28 23:31:16 +0200

    feat: add new Discord event 'preparation-surprise-2026-n-allez-pas-voir'
    - Generated using 'pcr event --name preparation-surprise-2026-n-allez-pas-voir'
    - File: preparation-surprise-2026-n-allez-pas-voir.js
    - Ready for implementation

---
+++ events/client/preparation-surprise-2026-n-allez-pas-voir.js
@@ -0,0 +1,11 @@
+// Événement: preparation-surprise-2026-n-allez-pas-voir
+//
+import { handleException, log } from '../../modules/utils.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const name = 'preparation-surprise-2026-n-allez-pas-voir';
+const once = false;
+async function execute() {}
+
+export { name, once, execute };
