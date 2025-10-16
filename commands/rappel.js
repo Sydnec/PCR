96ce2ccb8c1283d91b2e42e3c37cfec5320b8f58
Sydnec <sydnec@debian.dev.local>
2025-10-16 12:59:54 +0200

    feat: add new Discord command 'rappel'
    - Generated using 'pcr command --name rappel'
    - File: rappel.js
    - Ready for implementation

---
+++ commands/rappel.js
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { handleException } from '../modules/utils.js';
+import dotenv from 'dotenv';
+dotenv.config(); // process.env.CONSTANT
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('rappel')
+        .setDescription(`Description`)
+        .addStringOption((option) =>
+            option
+                .setName('option')
+                .setDescription('description')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        interaction.reply({
+            content: '',
+            ephemeral: true,
+        });
+    },
+};
