76392ab54d43e73a72f263ecab73da5236535bcb
sydnec <sydnec@host.lan>
2025-12-14 01:17:14 +0100

    feat: add /version command

---
+++ commands/version.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from "discord.js";
+import { handleException } from "../modules/utils.js";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("version")
+    .setDescription("Affiche la version actuelle du bot"),
+
+  async execute(interaction) {
+    try {
+      await interaction.reply({
+        content: `La version actuelle du bot est : **v${packageJson.version}**`,
+      });
+    } catch (error) {
+      handleException(error);
+    }
+  },
+};
