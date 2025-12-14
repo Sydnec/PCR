f6a7453c69bd920dbc77abb11448e2b56a288f50
sydnec <sydnec@host.lan>
2025-12-14 01:33:24 +0100

    fix: make version command ephemeral

--- commands/version.js
+++ commands/version.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, MessageFlags } from "discord.js";
 import { handleException } from "../modules/utils.js";
 import { readFileSync } from "fs";
 
